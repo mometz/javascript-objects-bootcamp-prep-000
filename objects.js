bd1683eaa7a0b58048ff4bd30b1102b3e6457572
Moritz M <moritz.muehlhausen@gmail.com>
2018-09-03 21:05:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = { Incubus: "Drive" }
+var playlist = { Phil Ochs: "d" }
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, {[artistName]: songTitle})
