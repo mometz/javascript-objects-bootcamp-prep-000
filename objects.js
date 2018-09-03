f33e40ab7049a993270ce6512b5fb2acf69c3b82
Moritz M <moritz.muehlhausen@gmail.com>
2018-09-03 21:00:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { RedHotChiliPeppers: "Otherside" }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName= "songTitle"
+  playlist.artistName = "songTitle"
   return playlist
 }
