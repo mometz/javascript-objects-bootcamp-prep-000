7234bdf5b8d53fca3b15e10136639fd9d0eb1744
Moritz M <moritz.muehlhausen@gmail.com>
2018-09-03 21:03:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,5 @@
 var playlist = { RedHotChiliPeppers: "Otherside" }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  Object.assign({}, playlist, {[artistName]: songTitle})
-  return playlist
+  return Object.assign({}, playlist, {[artistName]: songTitle})
 }
